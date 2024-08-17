8840abaa48b7a53403c335e0730bea99780f0bc2
xterzxc <valera.bondarev2005@gmail.com>
2024-08-17 22:07:42 +0200

    upd to working state, added upload animation

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import flet as ft
+from telethon import TelegramClient, events
+from test import api_id, api_hash, phone_number
+
+client = TelegramClient('imgtotext', api_id, api_hash)
+
+
+
+async def text_from_image(image_path):
+    '''
+    Using the Telegram API to send 
+    image to the bot and receive text response.
+    '''
+    await client.start(phone_number)
+
+    async with client.conversation('@imageToText_bot') as c:
+        await c.send_file(image_path)
+
+        response = await c.get_response()
+        return response.text
+
+
 
 def main(page: ft.Page):
-    page.title = "Catchy"
+    
     page.window.width = 800
     page.window.height = 600
     page.bgcolor = ft.colors.WHITE
@@ -16,7 +37,9 @@ def main(page: ft.Page):
         height=300,
     )
 
+
     def on_upload(e):
+        loading_indicator.visible = True 
         file_picker = ft.FilePicker(on_result=on_file_picked)
         page.overlay.append(file_picker)
 
@@ -24,17 +47,49 @@ def main(page: ft.Page):
 
         file_picker.pick_files(allow_multiple=False)
 
-    def on_file_picked(e: ft.FilePickerResultEvent):
+    async def on_file_picked(e: ft.FilePickerResultEvent):
         if e.files:
-            print("Selected file:", e.files[0].name)
+            image_path = e.files[0].path
+            extracted_text = await text_from_image(image_path)
+            result_text.value = extracted_text
+
+            result_text.visible = True
+            loading_indicator.visible = False
+            upload_btn.text = "Upload Another Image"
+
+            page.update()
+
+
 
     upload_btn = ft.ElevatedButton(text="Upload Image", on_click=on_upload)
+    loading_indicator = ft.ProgressRing(visible=False)
 
     title_text = ft.Text(
         value="Catchy",
         size=30,
         weight="bold",
+        color=ft.colors.BLACK,
         text_align=ft.TextAlign.CENTER,
+    )
+
+    result_text = ft.Text(
+        value="",
+        size=16,
+        text_align=ft.TextAlign.CENTER,
+    )
+
+    result_text = ft.TextField(
+        value="",
+        multiline=True,
+        label="Catchy",
+        width=500,
+        height=200,
+        read_only=True,
+        text_align=ft.TextAlign.LEFT,
+        border_color=ft.colors.BLUE,
+        focused_border_color=ft.colors.BLUE_ACCENT,
+        color=ft.colors.BLACK,
+        visible=False,
     )
 
 
@@ -44,6 +99,8 @@ def main(page: ft.Page):
                 icon,
                 title_text,
                 upload_btn,
+                loading_indicator,
+                result_text,
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
